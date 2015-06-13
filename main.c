bd24602689db3e4cdc972f0132d858588e883879
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2015-06-13 09:29:17 +0200

    Random multi-color blinker using interrupts

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/sleep.h>
 
 #include "debug.h"
 
@@ -8,40 +9,45 @@ int main(void) __attribute__((noreturn));
 
 int main(void)
 {
-	// disable interrupts
 	cli();
 
-	// set pin 5 of PORTB for output
-	DDRB |= _BV(DDB5);
+	/* set port B to output */
+	DDRB = 0xff;
 
-	// interrupt every 1s
-	OCR1A = F_CPU/1024;
+	/* Timer/Counter1 (the 16-bit one) setup */
+	const uint16_t TIMER1_HZ = 3;
+	TCCR1B =  _BV(WGM12) /* clear timer on compare match mode */
+		| _BV(CS12) | _BV(CS10); /* set prescaler to 1024 */
+	OCR1A = F_CPU/1024/TIMER1_HZ;
+	TIMSK1 |= (1 << OCIE1A); /* enable timer interrupt */
 
-	// mode 4, CTC on OCR1A
-	TCCR1B |= (1 << WGM12);
-
-	// set interrupt on compare match
-	TIMSK1 |= (1 << OCIE1A);
-
-	// set prescaler to 1024
-	TCCR1B |= (1 << CS12) | (1 << CS10);
-
-	// enable interrupts
-	sei();
-
-	for (;;) {}
+	for (;;) {
+		sleep_enable();
+		sei();
+		sleep_cpu();
+		sleep_disable();
+	}
 }
 
 ISR(TIMER1_COMPA_vect)
 {
 	static uint8_t state = 0;
-	state = !state;
+	state = 1337*state + 7;
+	PORTB = 0;
 
-	if (state) {
+	switch (state % 3) {
+	case 0:
+		PORTB |= _BV(PORTB3);
+		break;
+
+	case 1:
+		PORTB |= _BV(PORTB4);
+		break;
+
+	case 2:
 		PORTB |= _BV(PORTB5);
-		dprintf("Hello, ");
-	} else {
-		PORTB &= ~_BV(PORTB5);
-		dprintf("world!\n");
+		break;
 	}
+
+	dprintf("PORTB = %x\n", PORTB);
 }

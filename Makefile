05b35739bde32bc6d894f2485d6c40157ac53a25
Tomasz Dudziak <tomasz.dudziak@gmail.com>
2015-06-25 02:20:38 +0200

    Blinking a LED using Atomthreads RTOS

--- Makefile
+++ Makefile
@@ -1,12 +1,30 @@
 CC=avr-gcc
 BAUD=57600
-CFLAGS=-Os -std=c99 -Wall -fno-exceptions -ffunction-sections -fdata-sections -mmcu=atmega328p -DF_CPU=16000000L -DBAUD=${BAUD} -MMD -DUSB_VID=null
+ATOM_PATH=../atomthreads/
+
+CFLAGS=-Os -std=c99 -I$(ATOM_PATH)/kernel/ -I$(ATOM_PATH)/ports/avr -Wall -fno-exceptions -ffunction-sections -fdata-sections -mmcu=atmega328p -DF_CPU=16000000L -DBAUD=$(BAUD) -MMD -DUSB_VID=null
+
+KERNEL_OBJECTS = atomkernel.o atomsem.o atommutex.o atomtimer.o atomqueue.o
+PORT_OBJECTS = atomport.o atomport-asm.o
+APP_OBJECTS = main.o debug.o
 
 main.hex: main.elf
 	avr-objcopy -O ihex -R .eeprom $< $@
 
-main.elf: main.o debug.o
+main.elf: $(APP_OBJECTS) $(KERNEL_OBJECTS) $(PORT_OBJECTS)
 	avr-gcc -o $@ $(CFLAGS) $(LDFLAGS) $^
 
+$(KERNEL_OBJECTS): %.o: $(ATOM_PATH)/kernel/%.c
+	$(CC) -c $(CFLAGS) $< -o $(notdir $@)
+
+atomport.o: $(ATOM_PATH)/ports/avr/atomport.c
+	$(CC) -c $(CFLAGS) $< -o $(notdir $@)
+
+atomport-asm.o: $(ATOM_PATH)/ports/avr/atomport-asm.s
+	$(CC) -x assembler-with-cpp -c $(CFLAGS) $< -o $(notdir $@)
+
+$(APP_OBJECTS): %.o: %.c
+	$(CC) -c $(CFLAGS) $< -o $@
+
 program: main.hex
-	avrdude -patmega328p -carduino -P/dev/ttyUSB0 -b${BAUD} -Uflash:w:main.hex:i
+	avrdude -patmega328p -carduino -P/dev/ttyUSB0 -b$(BAUD) -Uflash:w:main.hex:i
--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <atom.h>
+#include <atomport-private.h>
+
 #include <stdint.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -6,48 +9,45 @@
 
 #include "debug.h"
 
-int main(void) __attribute__((noreturn));
+static ATOM_TCB main_tcb;
 
-/**
- * A pointless waste of memory but whatever...
- */
-uint8_t PATTERNS[] = {
-	/* go right to left */
-	_BV(0), _BV(1), _BV(2), _BV(3), _BV(4), _BV(5), _BV(6), _BV(7),
-	/* go left to right */
-	_BV(7), _BV(6), _BV(5), _BV(4), _BV(3), _BV(2), _BV(1), _BV(0),
-	/* start in the center and expand */
-	0x18, 0x3c, 0x7e, 0xff, 0xff, 0xff,
-	/* blink like crazy */
-	0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55,
-};
+static uint8_t main_thread_stack[256];
+static uint8_t idle_thread_stack[128];
 
-/**
- * There is an 8-bit M74HC595B1 shift register connected as follows:
- *
- *     PORTB3 (MOSI)  <->   SI
- *     PORTB5 (SCK)   <->   SCK
- *     PORTB2 (SS)    <->   RCK
- *
- * The program communicates with the shift register by using the SPI hardware
- * and does some blinking on the LEDs that are connected to the register.
- */
-int main(void)
+static void main_thread_func(uint32_t data)
 {
-	cli();
-
 	/* set port B to output */
 	DDRB = 0xff;
 
-	/* enable and configure SPI interface */
-	SPCR = _BV(SPE) | _BV(DORD) | _BV(MSTR) | _BV(SPR1);
-	SPDR = 0x00;
-
-	for (unsigned i = 0;; i++) {
-		PORTB ^= _BV(PORTB2);
-		SPDR = PATTERNS[i % sizeof(PATTERNS)];
-		_delay_ms(100);
-		PORTB ^= _BV(PORTB2);
-		_delay_ms(10);
+	for (;;) {
+		/* blink the Arduino LED */
+		PORTB ^= _BV(PORTB5);
+		_delay_ms(500);
 	}
+}
+
+int main(void)
+{
+	int8_t status;
+	dprintf("initializing...\n");
+
+	/* TODO: implement panics; check for ATOM_OK */
+	status = atomOSInit(
+		&idle_thread_stack[0],
+		sizeof(idle_thread_stack)/sizeof(idle_thread_stack[0]),
+		FALSE);
+
+	avrInitSystemTickTimer();
+
+	status = atomThreadCreate(
+		&main_tcb,
+		0,
+		main_thread_func,
+		0,
+		&main_thread_stack[0],
+		sizeof(main_thread_stack)/sizeof(main_thread_stack[0]),
+		TRUE);
+
+	atomOSStart();
+	return 0;
 }
